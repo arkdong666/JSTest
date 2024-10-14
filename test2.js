2e8e737d5720790faac7d0a4987ce2b60f0eded8
Ark <arkdong666@gmail.com>
2024-10-14 21:53:03 +0800

    feat(practice) 練習 String 查詢用法。

--- test2.js
+++ test2.js
@@ -26,8 +26,55 @@ console.log(history.length);
 console.clear();
 console.log("Hello");
 
+let num1 = 3.14;
+let num2 = 34;
+let num3 = 123e5;
+let num4 = 123e-5;
 
+console.log("num1= " + num1);
+console.log("num2= " + num2);
+console.log("num3= " + num3);
+console.log("num4= " + num4);
 
+let num = "dddd";
+console.log(num.constructor === String);
 
+let num5 = 100;
+console.log(num5.EPSILON); //undefined
+console.log(Number.EPSILON); //2.220446049250313e-16
 
+let num6 = 345.12645;
+console.log(typeof num6.toString()); //string
+
+console.log(num6.toExponential(2)); //3.45e+2
+console.log(num6.toExponential(4)); //3.4512e+2
+console.log(num6.toExponential(6)); //3.451200e+2
+
+let demo = "123.456";
+console.log(num6.toFixed()); //四捨五入到小數第2位
+console.log(typeof demo.valueOf());
+
+let num7 = 123.456;
+console.log(num7.toPrecision(5));
+
+let inTemp1 = "Kitty";
+let age = 16;
+let template = `Hello 
+World 
+${inTemp1}, I'm ${age + 1} years old.`;
+console.log(template);
+
+let texts = "I love JavaScript and you love JavaScript, too.";
+console.log(texts.indexOf("Java")); //7
+console.log(texts.indexOf("JavaScript")); //7
+console.log(texts.indexOf("JavaScript", 10)); //31(從索引10開始)
+console.log(texts.indexOf(1)); //-1(沒找到回傳-1 Number)
+console.log(texts.lastIndexOf("JavaScript")); //31
+console.log(texts.lastIndexOf("JavaScript", 6));  //由右向左搜尋
+console.log(texts.search("JavaScript")); //7
+console.log(texts.search(/JavaScript4/)); //7
+
+let dd = texts.match(/Java/g) //2
+console.log(dd);
+// console.log());
 
